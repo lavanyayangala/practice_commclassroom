605e66695abc2b972528d7dd7a1c2ad849b1ca78
lavanya yangala <lavanyayangala9981@gmail.com>
2022-01-26 01:12:58 +0530

    Files of programs-1st commit-FirstP program is amending

---
+++ src/com/lavs/FirstP.java
@@ -0,0 +1,11 @@
+package com.lavs;
+
+import java.util.Scanner;
+
+public class FirstP {
+    public static void main(String [] args){
+        System.out.println("First program");
+        System.out.println("hello this is lavs");
+        Scanner input = new Scanner(System.in);
+    }
+}
